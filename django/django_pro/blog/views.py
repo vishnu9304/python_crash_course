f1f7311f105de1f75a90d2992ede9c8a86a49e64
v01 <vishnu.pradeep@oath.com>
2020-02-29 21:04:23 +0530

    added templates

--- django/django_pro/blog/views.py
+++ django/django_pro/blog/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+posts = [
+    {
+        "author": "vishnu",
+        "title": "Blog Post 1",
+        "content": "first post content",
+        "date": "20-Feb-2020"
+    },
+    {
+        "author": "pradeep",
+        "title": "Blog Post 2",
+        "content": "second post content",
+        "date": "21-Feb-2020"
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        "posts": posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About Me</h1>')
+    return render(request, 'blog/about.html', {"title":"About"})
